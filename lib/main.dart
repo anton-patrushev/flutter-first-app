55410661bf8705831bfbcf0cbcabdafd2ada6ef6
anton-patrushev <anton11patrushev@gmail.com>
2023-07-20 18:40:47 +0200

    feat: implement first custom widget to reuse it

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,6 +39,18 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,25 +69,51 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Some text'),
-                ElevatedButton(
-                    onPressed: () {
-                      print("Called");
-                    },
-                    child: Text("Call me!"))
-              ],
-            )
+            CounterAction(
+                helpText: "You can also decrement it here",
+                icon: const Icon(CupertinoIcons.minus),
+                onPress: _decrementCounter),
+            CounterAction(
+                helpText: "You can also increment it here",
+                icon: const Icon(CupertinoIcons.plus),
+                onPress: _incrementCounter),
+            CounterAction(
+                helpText: "You can also reset it here",
+                icon: const Icon(CupertinoIcons.refresh),
+                onPress: _resetCounter),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(CupertinoIcons.add),
       ),
+    );
+  }
+}
+
+class CounterAction extends StatelessWidget {
+  const CounterAction(
+      {required this.helpText, required this.icon, required this.onPress});
+
+  final String helpText;
+  final Icon icon;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(
+            right: 20,
+          ),
+          child: Text(helpText),
+        ),
+        ElevatedButton(onPressed: onPress, child: icon)
+      ],
     );
   }
 }
